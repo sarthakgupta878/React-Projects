6c15c93090ce0b206d02bfdb569251cef9379da3
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-04-30 14:32:51 +0530

    starting new project to learn class components

---
+++ newsapp/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello World
+      </div>
+    )
+  }
+}
+
+
