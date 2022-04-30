3ee914ca4da8d47cbdedea72ceaa4a92d1958d49
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-04-30 15:45:36 +0530

    cpmponent structure created

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
+
   render() {
     return (
       <div>
-        Hello World
+        <NavBar/>
+        <News/>
       </div>
     )
   }
