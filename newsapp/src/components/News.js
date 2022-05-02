92f3a9bf392a9ca1fe67c8d1666aeb4f490cd4d2
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-02 14:15:08 +0530

    removed code redundancy by introducing functions

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -5,80 +5,73 @@ import PropTypes from 'prop-types'
 
 export class News extends Component {
     static defaultProps = {
-        country : 'in',
+        country: 'in',
         pageSize: 8,
-        category:'general',
+        category: 'general',
     }
 
-    static propTypes= {
-        country : PropTypes.string,
+    static propTypes = {
+        country: PropTypes.string,
         pageSize: PropTypes.number,
         category: PropTypes.string,
 
     }
 
-   articles =[];
-    constructor() {
-        super();
+    capitalizeFirstLetter = (string) => {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    articles = [];
+    constructor(props) {
+        super(props);
         // console.log("constructor of news component")
         this.state = {
             articles: this.articles,
             loading: false,
             page: 1
         }
+        document.title=`NewsTak Latest News - ${this.capitalizeFirstLetter(this.props.category)}`
+    }
+    async updateNews() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2859da7387a14fb99a820e5fffb7a43a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({ loading: true })
+        let data = await fetch(url);
+        let parsedData = await data.json()
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults, loading: false })
+
     }
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2859da7387a14fb99a820e5fffb7a43a&page=1&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
-
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        // console.log(parsedData);
-        
-        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults,loading:false })
-
+        this.updateNews();
     }
     handlePreviousClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2859da7387a14fb99a820e5fffb7a43a&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
-        let data = await fetch(url);
-        let parsedData = await data.json()
         this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading:false
+            page: this.state.page - 1
+        }, () => {
 
-        })
+            this.updateNews();
+        });
 
     }
     handleNextClick = async () => {
-
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2859da7387a14fb99a820e5fffb7a43a&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true})
-        
-        let data = await fetch(url);
-        let parsedData = await data.json()
         this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles,
-            loading:false
+            page: this.state.page + 1
+        }, () => {
 
-        })
-
-
+            this.updateNews();
+        });
 
     }
     render() {
         return (
             <div className="container my-3">
 
-                <h1 style={{margin:"30px 0px"}} className="text-center">NewsTak - Top Headlines</h1>
-                {this.state.loading && <Spinner/>}
+                <h1 style={{ margin: "30px 0px" }} className="text-center">NewsTak - Top Headlines from {this.capitalizeFirstLetter(this.props.category)}</h1>
+                {this.state.loading && <Spinner />}
                 {/* {this.state.articles.map((element)=>{console.log(element)})} */}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
                         return <div className="col-md-3" key={element.url}>
-                            <NewsItem source={element.source.name} title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 100) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author ={element.author} date={element.publishedAt} />
+                            <NewsItem source={element.source.name} title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 100) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} />
                         </div>
                     })}
                 </div>
