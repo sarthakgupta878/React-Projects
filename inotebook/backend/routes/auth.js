1ee8f3343bc01726c653af3bd70a82c5059f2137
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-07 23:24:17 +0530

    create user api compleated

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,25 +1,28 @@
-const express =require('express');
+const express = require('express');
 const router = express.Router();
-const User =require('../models/User')
-const {body, validationResult } = require('express-validator');
+const User = require('../models/User')
+const { body, validationResult } = require('express-validator');
 
-router.post('/',[
-    body('name','Enter a valid Name').isLength({min:3}),
-    body('password','Password must be 5 characters long').isLength({min:5}),
-    body('email','Enter a valid Email').isEmail(),
-],(req,res)=>{
-   const errors = validationResult(req);
-   if(!errors.isEmpty()){
-       return res.status(400).json({errors:errors.array()});
-   }
-   User.create({
-       name: req.body.name,
-       password: req.body.password,
-       email: req.body.email,
-   }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-        res.json({error:err.message})
-    })
+router.post('/createuser', [
+    body('name', 'Enter a valid Name').isLength({ min: 3 }),
+    body('password', 'Password must be 5 characters long').isLength({ min: 5 }),
+    body('email', 'Enter a valid Email').isEmail(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        let user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+        })
+        res.json(user)
+    }
+    catch (err) {
+        res.status(500).json({ error: err.message })
+    }
 })
 
-module.exports =router
+module.exports = router
