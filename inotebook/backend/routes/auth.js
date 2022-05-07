a82e13f7765d95215ea5199efdf4e56d13a8e4dc
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-07 18:20:50 +0530

    post request to insert a user created

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,8 +1,12 @@
 const express =require('express');
 const router = express.Router();
+const User =require('../models/User')
 
-router.get('/',(req,res)=>{
-
+router.post('/',(req,res)=>{
+    console.log(req.body)
+    const user = User(req.body);
+    user.save();
+    res.send("Hello")
 })
 
 module.exports =router
