b03c497eb46f7bb306d9c2fbf356e4605c690dbb
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-08 01:35:15 +0530

    created login endpoint

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -35,7 +35,42 @@ router.post('/createuser', [
         res.json({authToken})
     }
     catch (err) {
-        res.status(500).json({ error: err.message })
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+})
+
+router.post('/login', [
+   
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'password cannot be blank').exists(),
+
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password} = req.body;
+    try{
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"try to login with correct Credentials"});
+        }
+        const passwordCompare = await bcrypt.compare(password,user.password)
+        if(!passwordCompare){
+            return res.status(400).json({error:"try to login with correct Credentials"});
+        }
+
+        const data = {
+            user:{
+                id:user._id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        res.json({authToken})
+    }
+    catch(err){
+        res.status(500).json({ error: "Internal Server Error" })
+
     }
 })
 
