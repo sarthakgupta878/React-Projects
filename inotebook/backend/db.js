e5b2f4fbe8c3ab0f28da93e981d0c630f8fb1c3f
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-07 16:23:08 +0530

    connection established to mongodb atlas successful

---
+++ inotebook/backend/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb+srv://admin:z3BstGcDVsnkMGks@cluster0.gjzsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const connectToMongo = () => {
+    mongoose.connect(mongoURI,()=>{
+        console.log("Connected to mongo db atlas Succesfully")
+    })
+
+}
+
+module.exports = connectToMongo;
