7a5fc08aa7b2a6dceccf41b1ba7bb8ec99dd19be
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-08 00:43:24 +0530

    create bcrypt and jwt varification

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'kskalnasjaslbas';
 
 router.post('/createuser', [
     body('name', 'Enter a valid Name').isLength({ min: 3 }),
@@ -13,12 +17,22 @@ router.post('/createuser', [
         return res.status(400).json({ errors: errors.array() });
     }
     try {
+        const salt = await bcrypt.genSalt(10);
+       const secPass = await bcrypt.hash(req.body.password,salt)
         let user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
-        })
-        res.json(user)
+        });
+        const data = {
+            user:{
+                id: user._id
+            }
+        }
+      const authToken =  jwt.sign(data, JWT_SECRET);
+    
+
+        res.json({authToken})
     }
     catch (err) {
         res.status(500).json({ error: err.message })
