b78e09d1ba532c1324aa52ac5ec124f223f3d4b5
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-10 15:48:32 +0530

    created navbar and router setup

--- inotebook/src/App.js
+++ inotebook/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import About from './components/About';
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-     <h1>This is iNotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
