82163a585b4c760dda5c06c0b699079887d3b7d9
Sarthak Gupta <sarthakgupta878@gmail.com>
2022-05-17 17:07:04 +0530

    use location

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,15 +1,11 @@
-import React, { useEffect } from 'react'
-import { useContext } from 'react'
-import noteContext from '../context/notes/noteContext'
+import React from 'react'
 
 const About = () => {
-  const a = useContext(noteContext)
-  useEffect(()=>{
-    a.update()
-  },[])
+  
+  
   return (
     <div>
-        This is About {a.state.name} and he is in class {a.state.class}
+        This is About page
     </div>
   )
 }
--- inotebook/src/context/notes/NoteState.js
+++ inotebook/src/context/notes/NoteState.js
@@ -1,22 +1,11 @@
-import { useState } from "react";
+
 import NoteContext from "./noteContext";
 
 const NoteState = (props)=>{
-    const s1 = {
-        "name":"sarthak",
-        "class":"5g"
-    }
-    const [state, setState] = useState(s1);
-   const update = () =>{
-        setTimeout(()=>{
-            setState({
-                "name":"Larry",
-                "class":"10b"
-            })
-        },1000);
-    }
+   
+  
     return (
-        <NoteContext.Provider value = {{state:state,update:update}}>
+        <NoteContext.Provider value = {{}}>
             {props.children}
         </NoteContext.Provider>
     )
